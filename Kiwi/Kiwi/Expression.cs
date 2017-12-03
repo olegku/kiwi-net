962f86b04296d2557d7cb26f474ab78f32a8e5fa
olegku <kukumber@gmail.com>
2017-12-04 01:16:21 +0300

    Add Term and Expression

---
+++ Kiwi/Kiwi/Expression.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+
+namespace Kiwi
+{
+    public class Expression
+    {
+        public Expression(double constant = 0.0)
+        {
+            Terms = new List<Term>();
+            Constant = constant;
+        }
+
+        public Expression(Term term, double constant = 0.0)
+        {
+            Terms = new List<Term> {term};
+            Constant = constant;
+        }
+
+        public Expression(List<Term> terms, double constant = 0.0)
+        {
+            Terms = terms; // TODO this looks weird
+            Constant = constant;
+        }
+
+        public List<Term> Terms { get; }
+
+        public double Constant { get; }
+
+        // TODO should be converter to method, as getter is not trivial
+        public double Value
+        {
+            get
+            {
+                var result = Constant;
+                foreach (var term in Terms)
+                {
+                    result += term.Value;
+                }
+                return result;
+            }
+        }
+    }
+}
--- Kiwi/Kiwi/Variable.cs
+++ Kiwi/Kiwi/Variable.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace Kiwi
+﻿namespace Kiwi
 {
     public class Variable
     {
