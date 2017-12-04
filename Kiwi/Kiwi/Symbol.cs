99457a1905be0b02ddb02f4e4c1b2d5011959bb5
olegku <kukumber@gmail.com>
2017-12-05 01:19:26 +0300

    Add Symbol and Errors.

---
+++ Kiwi/Kiwi/Symbol.cs
@@ -0,0 +1,28 @@
+﻿namespace Kiwi
+{
+    public enum SymbolType
+    {
+        Invalid,
+        External,
+        Slack,
+        Error,
+        Dummy
+    };
+
+
+    public class Symbol
+    {
+        public SymbolType Type { get; }
+        public ulong Id { get; }
+
+        public Symbol()
+        {
+        }
+
+        public Symbol(SymbolType type, ulong id)
+        {
+            Id = id;
+            Type = type;
+        }
+    }
+}
